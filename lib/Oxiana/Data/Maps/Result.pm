46c1af9a2fb46bc27529b68de14eb2fe21f41f4d
simone cesano <simone.cesano@gmail.com>
2014-12-26 17:05:27 +0100

    Added readable/weriteable check

--- lib/Oxiana/Data/Maps/Result.pm
+++ lib/Oxiana/Data/Maps/Result.pm
@@ -9,4 +9,16 @@ use base 'DBIx::Class::Core';
 
 __PACKAGE__->load_components(qw{Helper::Row::ToJSON});
 
+sub readable_by {
+    my ($self, $relationship, $user) = @_;
+    my $o = $self->find_related($relationship, {});
+    return ((index($o->user_id, $user) >= 0) || (index($o->can_read, $user) >= 0) || (index($o->can_write, $user) >= 0))
+}
+
+sub writeable_by {
+    my ($self, $relationship, $user) = @_;
+    my $o = $self->find_related($relationship, {});
+    return ((index($o->user_id, $user) >= 0) || (index($o->can_write, $user) >= 0))
+}
+
 1;
