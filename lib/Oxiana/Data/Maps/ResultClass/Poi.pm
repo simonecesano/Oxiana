52ede77acaabb391298121e0cc4e9e7f8e6a5d24
simone cesano <simone.cesano@gmail.com>
2015-01-05 11:16:18 +0100

    First commit

---
+++ lib/Oxiana/Data/Maps/ResultClass/Poi.pm
@@ -0,0 +1,29 @@
+package Oxiana::Data::Maps::ResultClass::Poi;
+
+use base 'DBIx::Class::ResultSet';
+
+use strict;
+use warnings;
+no warnings qw/uninitialized/;
+
+use JSON;
+
+sub as_json {
+    my $self = shift;
+    $self = $self->search;
+    $self->result_class('DBIx::Class::ResultClass::HashRefInflator');
+
+    my $json = JSON->new;
+    return $json->pretty->encode([ $self->all ]);
+}
+
+sub foo {
+    return 'hallo'
+}
+
+sub writeable_by {
+    my $self = shift;
+    return $self->search(@_)
+}
+
+1
