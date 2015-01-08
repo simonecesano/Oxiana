2840e2c565ca957c4c4605829d4802c022ec5ea6
simone cesano <simone.cesano@gmail.com>
2015-01-08 07:15:54 +0100

    Added redirect to page prior to login

--- lib/Oxiana/Controller/Login.pm
+++ lib/Oxiana/Controller/Login.pm
@@ -26,15 +26,18 @@ sub index :Path :Args(0) {
     my $name = $c->req->params->{name};
 
     if ($name) {
-	if ($c->authenticate( {
-			       username => $c->request->params->{'name'},
-			       password => $c->request->params->{'password'}
-			      } )) {  
-	    $c->res->redirect($c->uri_for("/home"));
-	} else {
-	    $c->stash->{template} = \"sorry";
-	}
+	# if ($c->authenticate( {
+	# 		       username => $c->request->params->{'name'},
+	# 		       password => $c->request->params->{'password'}
+	# 		      } )) {  
+	#     $c->res->redirect($c->uri_for("/home"));
+	# } else {
+	#     $c->stash->{template} = \"sorry";
+	# }
     } else {
+	$c->session->{login_referer} ||= $c->req->referer;
+	$c->log->info(sprintf "login_referer %s\n", $c->req->referer);
+	$c->log->info(sprintf "login_referer in session %s\n", $c->session->{login_referer});
 	$c->stash->{template} = "login.tt2";
     }
 }
@@ -48,13 +51,15 @@ sub logout :Path('/logout') :Args(0) {
 sub oauth :Path Args(1) {
     my ($self, $c, $provider) = @_;
     unless ($c->req->params->{code}) {
+	$c->log->info(sprintf "without code - login_referer %s\n", $c->session->{login_referer});
 	$c->authenticate({ provider => $provider });
     } else {
+	$c->log->info(sprintf "with code - login_referer %s\n", $c->session->{login_referer});
 	$c->authenticate({ provider => $provider });
 	#------------------------------
 	# need to redirect somewhere
 	#------------------------------
-	$c->res->redirect($c->uri_for("/maps", $c->user->uid));
+	$c->res->redirect($c->session->{login_referer} || $c->uri_for("/maps", $c->user->uid));
 
 	# $c->res->body('authenticated! "'. $c->user->uid . '"');
     }
